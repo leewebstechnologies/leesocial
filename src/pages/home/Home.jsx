af72e76771d4286fc17b47f4e62cea77d0e9df35
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-03 07:34:29 -0700

    Hello world inserted in App.js

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import "./home.scss";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
